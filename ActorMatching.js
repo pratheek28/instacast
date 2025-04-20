35c84a9ac0d00a351827be3991add712cc4cbdf1
Pratheek Sunilkumar <66023535+pratheek28@users.noreply.github.com>
2025-04-19 23:11:33 -0700

    Update ActorMatching.js

--- ActorMatching.js
+++ ActorMatching.js
@@ -1,20 +1,41 @@
-import React from 'react';
-import styles from './ActorMatching.module.css';
+import React, { useRef } from 'react';
+import styles from './actormatching.module.css';
 
 function ActorMatching() {
-   return (
-    <div>
-        <div className={styles.container}>
-            <button className={styles.dislike}> </button>
-            <button className={styles.like}> </button>
-        </div>
-        <div>
-            <button className={styles.dashboard}>  </button>
-        </div>
-    </div>
-    
+    const boxRef = useRef(null);
 
-   )
+    const playAnimation = (animationName) => {
+        if (boxRef.current) {
+            console.log("Button is clicked:", animationName);
+            boxRef.current.style.animation = 'none';
+
+            requestAnimationFrame(() => {
+                void boxRef.current.offsetWidth; // Trigger reflow
+                boxRef.current.style.animation = `${animationName} 2.5s ease-in-out forwards`;
+            });
+        }
+    };
+
+    return (
+        <div>
+            <div className={styles.container}>
+                <button
+                    className={styles.dislike}
+                    id="dislike"
+                    name="dislike"
+                    onClick={() => playAnimation('move-left')} // Attach onClick handler
+                ></button>
+                <button
+                    className={styles.like}
+                    id="like"
+                    name="like"
+                    onClick={() => playAnimation('move-right')} // Attach onClick handler
+                ></button>
+            </div>
+
+            <div className={styles.tempBox} ref={boxRef}></div>
+        </div>
+    );
 }
 
 export default ActorMatching;
