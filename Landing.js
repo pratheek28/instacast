ee67d8400dc4f05fe92c6c0ce54ff3c252f37455
ssrin051 <31sanvisrinivas@gmail.com>
2025-04-19 22:35:57 -0700

    Create Landing.js

---
+++ Landing.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import styles from "./Landing.module.css";
+
+function Instacast() {
+    // need to handle login/signup button logic 
+
+    return (
+        <div className={styles.container}>
+            <button className={styles.login}> </button>
+            <button className={styles.signup}> </button>
+        </div>
+    )
+
+}
+
+export default Instacast;
