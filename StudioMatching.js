dc04b961425628405ccfac00ab783d049e1fce8f
ssrin051 <31sanvisrinivas@gmail.com>
2025-04-19 23:24:38 -0700

    Update StudioMatching.js

--- StudioMatching.js
+++ StudioMatching.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './StudioMatching.module.css';
 
 function StudioMatching() {
-   return (
-    <div>
-        <div className={styles.container}>
-            <button className={styles.dislike}> </button>
-            <button className={styles.like}> </button>
-
-            
-        </div>
+   const [swipeDirection, setSwipeDirection] = useState('');
+   
+    const handleSwipe = (direction) => {
+        setSwipeDirection(direction);
+   
+        setTimeout(() => setSwipeDirection(''), 800);
+    };
+   
+    return (
         <div>
-            <button className={styles.dashboard}>  </button>
+            <div className={styles.container}>
+                <button
+                    className={styles.dislike}
+                    onClick={() => handleSwipe('swipeLeft')}
+                ></button>
+                <button
+                    className={styles.like}
+                    onClick={() => handleSwipe('swipeRight')}
+                ></button>
+            </div>
+   
+            <div
+                className={`${styles.tempBox} ${swipeDirection ? styles[swipeDirection] : ''}`}
+            ></div>
         </div>
-    </div>
-
-
-   )
+    );
 }
 
 export default StudioMatching;
