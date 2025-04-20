50fedeaa5e9e2f3480928c5983a1fc71e43bb37a
ssrin051 <31sanvisrinivas@gmail.com>
2025-04-19 20:46:35 -0700

    Create StudioMatching.js

---
+++ StudioMatching.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './StudioMatching.module.css';
+
+function StudioMatching() {
+   return (
+    <div>
+        <div className={styles.container}>
+            <button className={styles.dislike}> </button>
+            <button className={styles.like}> </button>
+        </div>
+    </div>
+
+
+   )
+}
+
+export default StudioMatching;
